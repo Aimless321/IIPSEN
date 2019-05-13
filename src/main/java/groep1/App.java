2add9b1630c95093d19e2829614ecb30a394bdff
Aimless321 <6079485+Aimless321@users.noreply.github.com>
2019-05-13 12:30:13 +0200

    Add firestore connection

--- src/main/java/groep1/App.java
+++ src/main/java/groep1/App.java
@@ -1,13 +1,34 @@
 package groep1;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import com.google.api.core.ApiFuture;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.*;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
+import com.google.firebase.database.annotations.Nullable;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+
+public class App
 {
-    public static void main(String[] args)
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
+        InputStream serviceAccount =
+                App.class.getResourceAsStream("/firebase-creds.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setDatabaseUrl("https://iipsen-groep1.firebaseio.com")
+                .build();
+
+        FirebaseApp.initializeApp(options);
+
+        Firestore db = FirestoreClient.getFirestore();
     }
 }
