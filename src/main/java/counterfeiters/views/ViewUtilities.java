0a6091b94278f38fab478e489fe48b32c17313bc
Aimless321 <6079485+Aimless321@users.noreply.github.com>
2019-06-01 00:40:59 +0200

    Make MainMenuView responsive

--- src/main/java/counterfeiters/views/ViewUtilities.java
+++ src/main/java/counterfeiters/views/ViewUtilities.java
@@ -4,17 +4,21 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class ViewUtilities {
+    public static double screenWidth = Screen.getPrimary().getBounds().getWidth();
+    public static double screenHeight = Screen.getPrimary().getBounds().getHeight();
+
     public static Background getBackground(String path) {
         //Load image with classloader
         Image image = new Image(ViewUtilities.class.getResourceAsStream(path));
 
         //Set background size
-        BackgroundSize backgroundSize = new BackgroundSize(1920, 1080, false, false, false, false);
+        BackgroundSize backgroundSize = new BackgroundSize(1920, 1080, false, false, false, true);
 
         //Init backgroundImage
         BackgroundImage backgroundImage = new BackgroundImage(image, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
