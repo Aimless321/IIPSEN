3e0390286a3935f62210e9fc60694bccdd1121fd
Robin9506 <zhuxi@hotmail.nl>
2019-05-31 16:47:36 +0200

    ScoreboardView Made + changed starting view in Main

--- src/main/java/counterfeiters/main/App.java
+++ src/main/java/counterfeiters/main/App.java
@@ -1,6 +1,8 @@
 package counterfeiters.main;
 
+import counterfeiters.views.LobbyView;
 import counterfeiters.views.MainMenuView;
+import counterfeiters.views.ScoreboardView;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -12,7 +14,7 @@ public class App extends Application
 
     @Override
     public void start(Stage primaryStage) {
-        new MainMenuView(primaryStage);
+        new ScoreboardView(primaryStage);
 
         primaryStage.setMaximized(true);
         primaryStage.show();
