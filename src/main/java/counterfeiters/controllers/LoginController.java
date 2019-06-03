7519482c48306bdb080e4900a820b7d33d61c27e
Ali Rezaa <alirezaa_afg@live.nl>
2019-06-03 12:46:31 +0200

    add Login + Register Controller

---
+++ src/main/java/counterfeiters/controllers/LoginController.java
@@ -0,0 +1,9 @@
+package counterfeiters.controllers;
+
+public class LoginController {
+    private ApplicationController app;
+
+    public LoginController(ApplicationController applicationController) {
+        this.app = applicationController;
+    }
+}
--- src/main/java/counterfeiters/views/RegisterView.java
+++ src/main/java/counterfeiters/views/RegisterView.java
@@ -1,5 +1,7 @@
 package counterfeiters.views;
 
+import counterfeiters.controllers.LoginController;
+import counterfeiters.controllers.RegisterController;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -9,20 +11,22 @@ import javafx.stage.Stage;
 
 public class RegisterView implements Observer {
     private Stage stage;
+    private RegisterController controller;
 
     //Need an empty constructor for FXML
     public RegisterView() {
 
     }
 
-    public RegisterView(Stage primaryStage) {
+    public RegisterView(Stage primaryStage, Object registerController) {
         this.stage = primaryStage;
+        this.controller = (RegisterController) registerController;
 
         show();
     }
 
     public void show() {
-        Parent root = ViewUtilities.loadFxml("/views/register.fxml", stage);
+        Parent root = ViewUtilities.loadFxml("/views/register.fxml", stage, controller);
 
         //Find root pane and set background
         Pane pane = (Pane)root.lookup("Pane");
@@ -42,5 +46,10 @@ public class RegisterView implements Observer {
     @Override
     public void setStage(Stage stage) {
         this.stage = stage;
+    }
+
+    @Override
+    public void setController(Object controller) {
+
     }
 }
