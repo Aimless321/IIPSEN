98ef0d064430bf11ed0ea12bc79ed2cfc580e6b6
mmbasgol <mmbasgol@hotmail.com>
2019-06-06 08:36:12 +0200

    Layout van listView is nu goed cellFactory werkt.

--- src/main/java/counterfeiters/views/LobbyListView.java
+++ src/main/java/counterfeiters/views/LobbyListView.java
@@ -9,6 +9,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -17,9 +18,7 @@ import javafx.scene.control.ListView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.*;
 import javafx.scene.layout.GridPane;
 
 import javafx.scene.layout.HBox;
@@ -41,7 +40,7 @@ public class LobbyListView extends ListView<String> implements Observer,Initiali
     @FXML
     public ListView<String> list;
 
-    ObservableList<String> listView = FXCollections.observableArrayList("one", "two", "three");
+    //ObservableList<String> listView = FXCollections.observableArrayList("one", "two", "three");
 
     Image player_icon = new Image("icons/player.png");
 
@@ -70,8 +69,62 @@ public class LobbyListView extends ListView<String> implements Observer,Initiali
     public void show() {
         Parent root = ViewUtilities.loadFxml("/views/lobby_list.fxml", stage, controller);
 
+        ObservableList<ListRow> listRows = FXCollections.observableArrayList();
+
+        // Add a sample Chess piece, a queen in this case
+        listRows.addAll(new ListRow(
+                "12345",
+                new ImageView("icons/player.png"),"3")
+        ,new ListRow("1243567",new ImageView("icons/player.png"),"2"));
+
+        ListView<ListRow> lvListRow = new ListView<>();
+        lvListRow.setLayoutX(460.0);
+        lvListRow.setLayoutY(390.0);
+        lvListRow.setPrefHeight(400);
+        lvListRow.setPrefWidth(1000);
+
+        // Setup the CellFactory
+        lvListRow.setCellFactory(listView -> new ListCell<ListRow>() {
+            @Override
+            protected void updateItem(ListRow row, boolean empty) {
+                super.updateItem(row, empty);
+
+                if (empty) {
+                    setGraphic(null);
+                } else {
+
+
+                    Region region1 = new Region();
+                    HBox.setHgrow(region1, Priority.ALWAYS);
+
+                    Region region2 = new Region();
+                    HBox.setHgrow(region2, Priority.ALWAYS);
+
+                    HBox horBox = new HBox(new Label(row.getId()), region1, region2, row.getIcon(),new Label(" " + row.getPlayerAmount()+ "/4"));
+
+                    // Create a HBox to hold our displayed value
+                  //  HBox horBox = new HBox(5);
+                  //  horBox.setAlignment(Pos.CENTER_LEFT);
+
+                    // Add the values from our piece to the HBox
+                    //Label idInRow = new Label(row.getId());
+                    //horBox.getChildren().addAll(
+                     //       row.getIcon(),
+                    //        idInRow
+                           //new Label("x " + piece.getCount())
+                    //);
+
+                    // Set the HBox as the display
+                    setGraphic(horBox);
+                }
+            }
+        });
+
+        // Bind our list of pieces to the ListView
+        lvListRow.setItems(listRows);
         //Find root pane and set background
         Pane pane = (Pane) root.lookup("Pane");
+        pane.getChildren().add(lvListRow);
         pane.setBackground(ViewUtilities.getBackground("/background/standard.png"));
 
         Scene scene = new Scene(root, 1920, 1080);
@@ -121,11 +174,11 @@ public class LobbyListView extends ListView<String> implements Observer,Initiali
   //  }
 
     public void updateObservableList(List<QueryDocumentSnapshot> lobbies){
-        listView.removeAll();
+        //listView.removeAll();
 
-        for(int i=0; i<lobbies.size();i++) {
-            listView.add(lobbies.get(i).getId());
-        }
+       // for(int i=0; i<lobbies.size();i++) {
+       //     listView.add(lobbies.get(i).getId());
+       // }
 
     }
 
@@ -133,9 +186,36 @@ public class LobbyListView extends ListView<String> implements Observer,Initiali
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-        list.setItems(listView);
-        list.getSelectionModel().selectedItemProperty().addListener((ObservableValue<?extends String>ov, String old,String newV)->{});
+        //list.setItems(listView);
+        //list.getSelectionModel().selectedItemProperty().addListener((ObservableValue<?extends String>ov, String old,String newV)->{});
 
     }
 
+    class ListRow {
+        private String id;
+        private ImageView playerIcon;
+        private String playerAmount;
+
+        public ListRow(String id, ImageView playerIcon, String playerAmount) {
+            this.id = id;
+            this.playerIcon = playerIcon;
+            this.playerAmount = playerAmount;
+
+            //resize pic
+            this.playerIcon.setFitHeight(30);
+            this.playerIcon.setFitWidth(30);
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public ImageView getIcon() {
+            return playerIcon;
+        }
+
+        public String getPlayerAmount() {
+            return playerAmount;
+        }
+    }
 }
