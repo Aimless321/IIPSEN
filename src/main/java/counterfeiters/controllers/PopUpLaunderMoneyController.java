ad9b8218ae6b18cc98f2498ec3d542810e363a64
Ali Rezaa <alirezaa_afg@live.nl>
2019-06-12 13:05:55 +0200

    pop up kan geladen worden onclick

--- src/main/java/counterfeiters/controllers/PopUpLaunderMoneyController.java
+++ src/main/java/counterfeiters/controllers/PopUpLaunderMoneyController.java
@@ -1,4 +1,19 @@
 package counterfeiters.controllers;
 
+import counterfeiters.views.Observer;
+import counterfeiters.views.PopUpLaunderMoneyView;
+
 public class PopUpLaunderMoneyController {
+    private ApplicationController app;
+
+    public void launderMoney() {
+        app.loadView(PopUpLaunderMoneyView.class, app.popUpLaunderMoneyController);
+    }
+
+    public PopUpLaunderMoneyController(ApplicationController app) {
+        this.app = app;
+    }
+
+    public void registerObserver(Observer observer) {app.accountController.registerObserver(observer);
+    }
 }
